20dbe51a7b7a8a8c583c8fedcbd2cb4513bef8db
jojji1 <121304245+jojji1@users.noreply.github.com>
2022-12-23 23:19:31 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My Web Page</title>
+  </head>
+  <body>
+    <!-- Your HTML content goes here -->
+    <script>
+      // Your JavaScript code goes here
+      alert("Food that always make your belly happy");
+    </script>
+  </body>
+</html>
